8f84b826055991375ea94299f961806ff860c35b
Yann Esposito <yann.esposito@gmail.com>
2012-08-06 16:26:22 +0200

    Added some comments

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -12,14 +12,7 @@ where
 import Import
 import Handler.Helper
 import Yesod.Auth
-
--- entryAForm :: AForm App App Entry
--- entryAForm = Entry
---   <$> areq textField "Creator" Nothing
---   <*> areq textField "Title" Nothing
---   <*> areq urlField "Url" Nothing
---   <*> areq intField "Yeah" 0
---   <*> areq intField "Neah" 0
+import Data.Maybe (isNothing)
 
 data PersonRequest = PersonRequest {
                       title :: Text
@@ -37,28 +30,37 @@ entryForm = renderDivs  $ PersonRequest
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
+
+-- the name getHomeR is for
+-- handle the request GET on the resource HomeR
 getHomeR :: Handler RepHtml
 getHomeR = do
+  -- We get the current user id (return Nothing if not logged in)
   currentUserId <- maybeAuthId
+
+  -- Generate a couple
+  -- widget => some HTML containing the form labels and input fields
+  -- enctype => the encoding
   (widget,enctype) <- generateFormPost entryForm
+
+  -- We get the list of entries sorted by entry title
   entries <- runDB $ selectList [] [Desc EntryTitle]
+
+  -- We return some HTML (not full)
   defaultLayout $ do
     aDomId <- lift newIdent
     setTitle "Inmanis"
     $(widgetFile "homepage")
-  where
-    isNothing :: Maybe a -> Bool
-    isNothing Nothing = True
-    isNothing _ = False
 
-
+-- When we receive a post request on HomeR resource (/ path)
+-- We create a new resource
 postHomeR :: Handler RepHtml
 postHomeR = do
   currentUserId <- maybeAuthId
   case currentUserId of
     Nothing -> errorPage "You're not logged"
     _ -> do -- errorPage "You're logged"
-      ((res,entryWidget),enctype) <- runFormPost entryForm
+      ((res,_),_) <- runFormPost entryForm
       case res of
         FormSuccess personRequest -> do
           let newEntry = Entry currentUserId (title personRequest) (url personRequest) 0 0
@@ -74,8 +76,8 @@ getEntryR entryId = do
   -- maybe "" entryTitle maybeEntry
   -- if maybeEntry is Nothing returns ""
   -- else returns (entryTitle maybeEntry)
-  let title = maybe "" entryTitle maybeEntry
-  errorPageJson title
+  let titleEntry = maybe "" entryTitle maybeEntry
+  errorPageJson titleEntry
 
 postEntryR :: EntryId -> Handler RepHtmlJson
 postEntryR = undefined
