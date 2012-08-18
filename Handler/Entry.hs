255518ae1fdbda56fe56026d96f01c943b43b9a0
Yann Esposito (Yogsototh) <Yann.Esposito@gmail.com>
2012-08-18 21:52:30 +0200

    separated Entry + entry template

---
+++ Handler/Entry.hs
@@ -0,0 +1,118 @@
+module Handler.Entry (
+    getEntryR
+  , postEntryR
+  , putEntryR
+  , deleteEntryR
+  )
+where
+
+import Import
+import Handler.Helper
+import Yesod.Auth
+import Data.Maybe
+import Data.Text (pack)
+
+
+getEntryR :: EntryId -> Handler RepHtmlJson
+getEntryR entryId = do
+  currentUserId <- maybeAuthId
+  maybeEntry <- runDB $ get entryId
+    -- maybe "" entryTitle maybeEntry
+    --    if maybeEntry is Nothing returns ""
+    --    else returns (entryTitle maybeEntry)
+    -- errorPageJson titleEntry
+  let
+      titleEntry = maybe "This entry does not exist" entryTitle maybeEntry
+      jsonWidget = object ["msg" .= titleEntry]
+      htmlWidget = do
+        setTitle $ toHtml titleEntry
+        $(widgetFile "entry")
+  defaultLayoutJson htmlWidget jsonWidget
+
+
+testLogged ::  (UserId -> Handler RepHtmlJson) -> Handler RepHtmlJson
+testLogged v = do
+  maybeUserId <- maybeAuthId
+  case maybeUserId of
+    Nothing -> errorPageJson "You're not logged"
+    Just currentUserId -> (v currentUserId)
+
+postEntryR :: EntryId -> Handler RepHtmlJson
+postEntryR entry = do
+  testLogged $ \userId -> do
+    req <- runRequestBody
+    let  yeah = lookup "yeah" (fst req)
+         neah = lookup "neah" (fst req)
+    case yeah of
+      Nothing ->
+        case neah of
+          Nothing -> errorPageJson $ "Neither Yeah nor Neah!"
+          _       -> downvote userId entry
+      _ -> upvote userId entry
+
+downvote :: UserId -> EntryId -> Handler RepHtmlJson
+downvote = setVoteValue (-1)
+
+upvote :: UserId -> EntryId -> Handler RepHtmlJson
+upvote = setVoteValue 1
+
+setVoteValue :: Int -> UserId -> EntryId -> Handler RepHtmlJson
+setVoteValue value user entry = do
+  votes  <- runDB $ selectList [VoteEntry ==. entry,VoteCreator ==. user]
+                                [LimitTo 1]
+  msg <- insertOrUpdateVote votes
+  errorPageJson $ pack msg
+  where
+
+    -- Depending of votes insert a new vote
+    -- update the existing one.
+    -- Also must synchronize the total number of yeah/neah for the entry
+    --
+    -- Insert case
+    insertOrUpdateVote [] = do
+      _ <- runDB $ do
+        _ <- insert $ Vote user entry value
+        case value of
+           1    -> update entry [EntryYeah +=. 1]
+           (-1) -> update entry [EntryYeah +=. 1]
+           _ -> return ()
+      return "inserted"
+
+    -- Update case
+    insertOrUpdateVote (vote:_) = do
+          runDB $ updateVote oldvalue value (entityKey vote)
+          return "updated"
+          where
+            oldvalue = (voteValue $ entityVal vote)
+
+    updateVote  0 1 key = do
+          update key [VoteValue =. 1]
+          update entry [EntryYeah +=. 1]
+    updateVote 0 (-1) key = do
+          update key [VoteValue =. (-1)]
+          update entry [EntryNeah +=. 1]
+
+    updateVote  1 1 key = do
+          update key [VoteValue =. 0]
+          update entry [EntryYeah -=. 1]
+    updateVote  1 (-1) key = do
+          update key [VoteValue =. (-1)]
+          update entry [EntryYeah -=. 1]
+          update entry [EntryNeah +=. 1]
+
+    updateVote  (-1) 1 key = do
+          update key [VoteValue =. 1]
+          update entry [EntryNeah -=. 1]
+          update entry [EntryYeah +=. 1]
+    updateVote (-1) (-1) key = do
+          update key [VoteValue =. 0]
+          update entry [EntryNeah -=. 1]
+    updateVote _ _ _ = return ()
+
+
+
+
+putEntryR :: EntryId -> Handler RepHtmlJson
+putEntryR = undefined
+deleteEntryR :: EntryId -> Handler RepHtmlJson
+deleteEntryR = undefined
